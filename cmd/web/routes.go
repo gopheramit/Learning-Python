f39a37ae92f9c47defd8bb7d2f05dc77d4e2d7b4
gopheramit <amitagrawal10@outlook.com>
2021-10-02 10:07:19 +0530

    goth err

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,14 +1,73 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/bmizerany/pat"
+	"github.com/gorilla/sessions"
+
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
 )
 
 func (app *application) routes() http.Handler {
+	key1 := "DoN4QZCXaa3TJfr4BJZMQZNo"
+	maxAge := 86400 * 30 // 30 days
+	isProd := false      // Set to true when serving over https
+	store := sessions.NewCookieStore([]byte(key1))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.Secure = isProd
+	gothic.Store = store
+	goth.UseProviders(
+		google.New("263741611747-2bgmmh2vnbjvt02c3m8s30ujbb76obgf.apps.googleusercontent.com", "DoN4QZCXaa3TJfr4BJZMQZNo", "http://localhost:4000/auth/google/callback", "email", "profile"),
+	)
 	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(app.home))
-	//mux.Get("/", app.home)
+	//mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/login", http.HandlerFunc(app.Login))
+	mux.Get("/aboutus", http.HandlerFunc(app.AboutUs))
+	mux.Get("/privacypolicy", http.HandlerFunc(app.PrivacyPolicy))
+	mux.Get("/task", http.HandlerFunc(app.Task))
+	mux.Get("/techcontent", http.HandlerFunc(app.TechContent))
+
+	mux.Get("/auth/:provider/callback", http.HandlerFunc(app.auth))
+	mux.Get("/auth/:provider", http.HandlerFunc(gothic.BeginAuthHandler))
+	mux.Get("/", http.HandlerFunc(app.showTemplates))
+
 	return mux
+}
+func (app *application) auth(res http.ResponseWriter, req *http.Request) {
+	log.Println("In AUTH")
+	user, err := gothic.CompleteUserAuth(res, req)
+	if err != nil {
+		fmt.Fprintln(res, err)
+		return
+	}
+	log.Println("Completed with AUTH")
+	t, err := template.ParseFiles("success.html")
+	if err != nil {
+		fmt.Println(err)
+		res.WriteHeader(http.StatusInternalServerError)
+	}
+	log.Println("Parsed the template")
+	t.Execute(res, user)
+}
+
+func beginAuth(res http.ResponseWriter, req *http.Request) {
+	gothic.BeginAuthHandler(res, req)
+}
+
+func (app *application) showTemplates(res http.ResponseWriter, req *http.Request) {
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		fmt.Println(err)
+		res.WriteHeader(http.StatusInternalServerError)
+	}
+	t.Execute(res, false)
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/gopheramit/Learning-Python
 
 go 1.16
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/markbates/goth v1.68.0 // indirect
+)
